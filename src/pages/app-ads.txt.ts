27c94b594408243543b1f7a5539b277a7556ad4a
TXAVLOG <viptretrauc@gmail.com>
2026-08-03 10:23:27 +0700

    chore: update assetlinks and app-ads.txt

---
+++ src/pages/app-ads.txt.ts
@@ -0,0 +1,7 @@
+export const GET = () => {
+  return new Response("google.com, pub-1543189450912703, DIRECT, f08c47fec0942fa0", {
+    headers: {
+      "content-type": "text/plain; charset=utf-8"
+    }
+  });
+};
